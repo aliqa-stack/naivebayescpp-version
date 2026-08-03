de109906a9961cc570865bc951ba1d589c7241c0
aliqa-stack <alikalmaliki51@gmail.com>
2026-08-03 19:19:35 +0700

    probability

--- main.cpp
+++ main.cpp
@@ -2,13 +2,18 @@
 #include <sstream>
 #include <vector>
 #include <map>
+#include <cmath>
 
 class NaiveBayes{
     public:
-    std::map<std::string, int> countOf_data;
-    std::map<std::string, int> vocabUnik;
-    int total_data = 0;
-   // std::map<std::string, int> countOf_data2;
+    std::map<std::string, int> countOf_Pspeech;
+    std::map<std::string, int> countOf_Hspeech;
+    std::map<std::string, bool> vocabUnik;
+    int total_Pspeech = 0;
+    int total_Hspeech = 0;
+    int totalPword = 0;
+    int totalHword = 0;
+    // std::map<std::string, int> countOf_data2;
 
 };
 
@@ -16,8 +21,9 @@ class DataSet : public NaiveBayes{
     public:
     std::string data1;
     std::string data2;
-      
-    std::string tokinisasi(std::string& data){
+    
+    std::string tokinisasiData1(std::string& data){
+        int vocabUnikSize = vocabUnik.size();
         std::stringstream ss(data);
         std::string token;
         std::vector<std::string> tokens;
@@ -26,19 +32,72 @@ class DataSet : public NaiveBayes{
         }
         for(auto& t : tokens){
             std::cout<<t<<"\n";
-            countOf_data[t]++;
-            vocabUnik[t]++;
-            if(vocabUnik[t]==1 && vocabUnik.size() > 4){
-                continue;
-            }
-            std::cout<<"kata yang keluar:"<<vocabUnik[t]<<"\n";
+            countOf_Pspeech[t]++;
+            vocabUnik[t] = true;
+         
+           // std::cout<<"kata yang keluar:"<<vocabUnik[t]<<"\n";
 
             
-            total_data++;
-            //std::cout<<"jumlah kata yang keluar:"<<countOf_data[t]<<"/"<<total_data<<"\n";
+            total_Pspeech++;
+            totalPword++;
+            std::cout<<"jumlah kata yang keluar:"<<countOf_Pspeech[t]<<"/"<<total_Pspeech<<"\n";
         }
         return tokens[0];
     }
+
+    std::string tokinisasiData2(std::string& data){
+        int vocabUnikSize = vocabUnik.size();
+        std::stringstream ss(data);
+        std::string token;
+        std::vector<std::string> tokens;
+        while(getline(ss, token, ' ')){
+            tokens.push_back(token);
+        }
+        for(auto& t : tokens){
+            std::cout<<t<<"\n";
+            countOf_Hspeech[t]++;
+            vocabUnik[t] = true;
+           
+           std::cout<<"kata yang keluar:"<<vocabUnikSize<<"\n";
+           
+           
+           total_Hspeech++;
+           totalHword++;
+            std::cout<<"jumlah kata yang keluar:"<<countOf_Hspeech[t]<<"/"<<total_Hspeech<<"\n";
+        }
+        return tokens[0];
+
+    }
+
+    std::string probability(std::string& data){
+        float naiveClass = total_Pspeech + total_Hspeech;
+        float Pspeech = std::log((float)(total_Pspeech + 1) / (float)(naiveClass + 1));
+        float Hspeech = std::log((float)(total_Hspeech + 1) / (float)(naiveClass + 1));
+        
+        float Logp = Pspeech;
+        float Logh = Hspeech;
+        
+        std::vector<std::string> words = {tokinisasiData1(data)};
+        
+        int vocabUnikSize = vocabUnik.size();
+        for(auto& v : words){
+            int priorx = countOf_Pspeech[v] ? countOf_Pspeech[v] : 0;
+            int priory = countOf_Hspeech[v] ? countOf_Hspeech[v] : 0;
+       
+              float likehoodP = std::log((float)(priorx + 1)/ (float)(totalPword + vocabUnikSize + 1));
+              float likehoodH = std::log((float)(priory + 1)/ (float)(totalHword + vocabUnikSize + 1));
+
+              Logh += likehoodH;
+              Logp += likehoodP;
+
+              std::cout<<"Logp: "<<Logp<<"\n";
+              std::cout<<"Logh: "<<Logh<<"\n";
+
+              
+        }
+
+        return Logp > Logh ? "politic speech" : "non politic speech";
+            }
 
     std::string OutputData(std::string& data1){
         std::cout<< "Data 1: " << data1 << std::endl;
@@ -56,8 +115,9 @@ int main(){
     "Jadi saya tidak sebut, ya, Menteri-menteri. Ini Menteri-menteri ini jago-jago semua ini, Menteri-Menteri saya, Menko-Menko saya semua, ya. Kepala Badan, Wakil Menterinya juga jagoan semua, ya. Tadi Menteri UMKM juga kumisnya gagah sekali, kok, itu, dari Kerala keturunannya. Dia senang, Perdana Menteri Modi datang dia senang sekali itu. Saya kira kau keturunan Gujarat, ternyata bukan. Kerala, Kerala, pidatoP";
     data.data2 = "data ini berbeda dengan data1 untuk menguji algoritma naive bayes, nanti nya data ini akan diubah, pidato";
 
-    data.tokinisasi(data.data1);
-    //data.tokinisasi(data.data1);
+   // data.tokinisasiData1(data.data1);
+    data.probability(data.data1);
+    //data.tokinisasiData2(data.data2);
     
 
 
